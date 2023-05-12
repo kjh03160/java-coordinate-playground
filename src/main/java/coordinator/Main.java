aec381985a5393dbd3edea055bec443a55805152
juna.k <juna.k@navercorp.com>
2023-05-12 23:39:17 +0900

    feat: 사용자의 입력값을 검증하고 다시 입력 받을 수 있도록 한다.

--- src/main/java/coordinator/Main.java
+++ src/main/java/coordinator/Main.java
@@ -5,8 +5,18 @@ import coordinator.view.InputView;
 
 public class Main {
 	public static void main(String[] args) {
-		String input = InputView.input();
-		int[] points = StringUtils.splitCoordinateString(input);
+		Coordinate coordinate1, coordinate2;
+		while (true) {
+			String input = InputView.input();
+			int[] points = StringUtils.splitCoordinateString(input);
+			try {
+				coordinate1 = new Coordinate(points[0], points[1]);
+				coordinate2 = new Coordinate(points[2], points[3]);
+				break;
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
 
 	}
 }
