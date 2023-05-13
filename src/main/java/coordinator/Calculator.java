d3eb32d35e832071d1e3a5141b61addb13938ea1
juna.k <juna.k@navercorp.com>
2023-05-13 21:34:06 +0900

    feat: 사각형의 넓이를 계산한다.

--- src/main/java/coordinator/Calculator.java
+++ src/main/java/coordinator/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
 		return Math.sqrt(extractAndSquare(c1.getX(), c2.getX()) + extractAndSquare(c1.getY(), c2.getY()));
 	}
 
+	public static double calculateArea(double width, double height) {
+		return width *  height;
+	}
+
 	private static double extractAndSquare(Point p1, Point p2) {
 		return Math.pow(p1.extract(p2), 2);
 	}
--- src/test/java/coordinator/CalculatorTest.java
+++ src/test/java/coordinator/CalculatorTest.java
@@ -11,4 +11,10 @@ class CalculatorTest {
 		double result = Calculator.calculateDistance(new Coordinate(10, 10), new Coordinate(14, 15));
 		Assertions.assertThat(result).isEqualTo(6.403124, Offset.offset(0.00099));
 	}
+
+	@Test
+	void 사각형의_면적을_계산한다() {
+		double result = Calculator.calculateArea(10, 5);
+		Assertions.assertThat(result).isEqualTo(50);
+	}
 }
