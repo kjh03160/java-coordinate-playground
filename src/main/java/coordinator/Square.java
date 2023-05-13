bb77c6871e44d6f2d08d17b08d19073f0025a7ec
juna.k <juna.k@navercorp.com>
2023-05-13 22:44:58 +0900

    feat: 직사각형의 좌표만을 허용한다.

--- src/main/java/coordinator/Square.java
+++ src/main/java/coordinator/Square.java
@@ -1,20 +1,67 @@
 package coordinator;
 
+import java.util.List;
+
 public class Square {
 	private Coordinate leftDownCoordinate;
 	private Coordinate leftUpCoordinate;
 	private Coordinate rightDownCoordinate;
 	private Coordinate rightUpCoordinate;
 
-	public Square(Coordinate leftDownCoordinate, Coordinate leftUpCoordinate, Coordinate rightDownCoordinate, Coordinate rightUpCoordinate) {
-		this.leftDownCoordinate = leftDownCoordinate;
-		this.leftUpCoordinate = leftUpCoordinate;
-		this.rightDownCoordinate = rightDownCoordinate;
-		this.rightUpCoordinate = rightUpCoordinate;
+	public Square(List<Coordinate> coordinates) {
+		sort(coordinates);
+		validate(coordinates);
+		this.leftDownCoordinate = coordinates.get(0);
+		this.rightDownCoordinate = coordinates.get(1);
+		this.leftUpCoordinate = coordinates.get(2);
+		this.rightUpCoordinate = coordinates.get(3);
 	}
 
 	public double area() {
 		return Calculator.calculateDistance(leftDownCoordinate, rightDownCoordinate)
 			* Calculator.calculateDistance(leftDownCoordinate, leftUpCoordinate);
+	}
+
+	private void validate(List<Coordinate> coordinates) {
+		if (coordinates.size() < 4) {
+			throw new IllegalArgumentException("4개의 좌표가 필요합니다.");
+		}
+		Coordinate leftDown = coordinates.get(0);
+		Coordinate rightDown = coordinates.get(1);
+		Coordinate leftUp = coordinates.get(2);
+		Coordinate rightUP = coordinates.get(3);
+
+		if (!(leftDown.getX().equals(leftUp.getX()) && leftDown.getY().equals(rightDown.getY())
+			&& leftUp.getY().equals(rightUP.getY()) && rightUP.getX().equals(rightDown.getX()))) {
+			throw new IllegalArgumentException("직사각형이 아닙니다.");
+		}
+	}
+
+	private void sort(List<Coordinate> coordinates) {
+		coordinates.sort((o1, o2) -> {
+			if (o1.getX().extract(o2.getX()) > 0) {
+				return 1;
+			}
+			if (o1.getY().extract(o2.getY()) > 0) {
+				return 1;
+			}
+			return -1;
+		});
+	}
+
+	Coordinate getLeftDownCoordinate() {
+		return leftDownCoordinate;
+	}
+
+	Coordinate getLeftUpCoordinate() {
+		return leftUpCoordinate;
+	}
+
+	Coordinate getRightDownCoordinate() {
+		return rightDownCoordinate;
+	}
+
+	Coordinate getRightUpCoordinate() {
+		return rightUpCoordinate;
 	}
 }
--- src/test/java/coordinator/SquareTest.java
+++ src/test/java/coordinator/SquareTest.java
@@ -1,18 +1,57 @@
 package coordinator;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 
 class SquareTest {
+
+	@Test
+	void 사각형을_생성한다() {
+		Coordinate c1 = new Coordinate(0, 0);
+		Coordinate c2 = new Coordinate(4, 0);
+		Coordinate c3 = new Coordinate(0, 5);
+		Coordinate c4 = new Coordinate(4, 5);
+
+		Square square = new Square(Lists.newArrayList(c4, c2, c3, c1));
+
+		Assertions.assertThat(square.getLeftDownCoordinate()).isEqualTo(c1);
+		Assertions.assertThat(square.getRightDownCoordinate()).isEqualTo(c2);
+		Assertions.assertThat(square.getLeftUpCoordinate()).isEqualTo(c3);
+		Assertions.assertThat(square.getRightUpCoordinate()).isEqualTo(c4);
+	}
+
+	@Test
+	void 좌표가_4개가_아닐경우_사각형을_생성할_수_없다() {
+		Coordinate c1 = new Coordinate(0, 0);
+		Coordinate c2 = new Coordinate(4, 0);
+		Coordinate c3 = new Coordinate(0, 5);
+
+		Assertions.assertThatThrownBy(() -> new Square(Lists.newArrayList(c1, c2, c3)))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("4개의 좌표가 필요합니다.");
+	}
+
+	@Test
+	void 직사각형의_좌표가_아닌_경우_사각형을_생성할_수_없다() {
+		Coordinate c1 = new Coordinate(0, 0);
+		Coordinate c2 = new Coordinate(4, 0);
+		Coordinate c3 = new Coordinate(0, 5);
+		Coordinate c4 = new Coordinate(4, 6);
+
+		Assertions.assertThatThrownBy(() -> new Square(Lists.newArrayList(c1, c2, c3, c4)))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("직사각형이 아닙니다.");
+	}
 
 	@Test
 	void 사각형의_면적을_구한다() {
 		Coordinate c1 = new Coordinate(0, 0);
 		Coordinate c2 = new Coordinate(4, 0);
 		Coordinate c3 = new Coordinate(0, 5);
-		Coordinate c4 = new Coordinate(4, 6);
+		Coordinate c4 = new Coordinate(4, 5);
 
-		Square square = new Square(c1, c3, c2, c4);
+		Square square = new Square(Lists.newArrayList(c4, c2, c3, c1));
 
 		Assertions.assertThat(square.area()).isEqualTo(20);
 	}
